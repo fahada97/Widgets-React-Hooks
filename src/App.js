b83f1d7eef06f2cd1c7a26037dfa35b301a5848c
fahada97 <syedfahad.ahmed97@gmail.com>
2020-07-02 15:10:30 -0400

    created the accordion and search widget

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./Components/Accordion";
+import Search from "./Components/Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end JS framework.'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is one of the favorite JS libraries among engineers.'
+    },
+    {
+        title: 'How to use React?',
+        content: 'Use react by creating components.'
+    }
+]
+
+export default () => {
+    return (
+        <div>
+            {/* <Accordion items ={items} /> */}
+            <Search/>
+        </div>
+    )
 }
-
-export default App;
